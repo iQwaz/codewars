cb6542a15989b0c3bc225b3f72a1cf0c4606d0d1
Kirill Kozedub <dev@kozedub.ru>
2021-09-10 23:34:54 +0300

    refactor: fix linter errors

--- src/replace_with_alphabet_position.js
+++ src/replace_with_alphabet_position.js
@@ -1,14 +1,14 @@
 function alphabetPosition(text) {
-  const alfabet = "abcdefghijklmnopqrstuvwxyz"
-  let result = "";
+  const alfabet = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
   for (let i = 0; i < text.length; i += 1) {
-    text = text.toLowerCase();
-    if (alfabet.indexOf(text[i]) !== -1) {
-      result += alfabet.indexOf(text[i]) + 1;
-      result += " ";
+    const textLowerCase = text.toLowerCase();
+    if (alfabet.indexOf(textLowerCase[i]) !== -1) {
+      result += alfabet.indexOf(textLowerCase[i]) + 1;
+      result += ' ';
     }
   }
   return result.trim();
 }
 
-console.log(alphabetPosition("The sunset sets at twelve o' clock.")) // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
