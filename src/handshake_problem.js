d2fdc5b9e1149ebe3bd02cf9dbc72ce1fcd61ee5
Kirill Kozedub <dev@kozedub.ru>
2021-09-13 12:13:32 +0300

    feat: solve handshake-problem

---
+++ src/handshake_problem.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/handshake-problem
+
+const getParticipants = (handshakes) => {
+  let farmers = 1;
+  while (farmers * (farmers - 1) / 2 < handshakes){
+    farmers += 1;
+  }
+  return farmers;
+}
+
+console.log(getParticipants(45)); // 10
