82492f9af9bdfb0f5ea4b2a9dafb89f3e896c16f
Kirill Kozedub <dev@kozedub.ru>
2021-09-13 14:15:00 +0300

    feat: solve multiples-of-3-or-5

---
+++ src/multiples_of_3_or_5.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/multiples-of-3-or-5
+
+const solution = (number) => {
+  if (number <= 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 1; i < number; i += 1) {
+    if (( i % 3 === 0) || (i % 5 === 0)) {
+      sum += i;
+      console.log(i);
+    }
+  }
+  return sum;
+}
+
+console.log(solution(10)); // 23
