6d0c34184478b4206555f582328d4134124b299b
Kirill Kozedub <dev@kozedub.ru>
2021-09-17 16:01:13 +0300

    feat: solve head-tail-init-and-last

---
+++ src/head_tail_init_and_last.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/head-tail-init-and-last
+
+const head = (arr) => arr[0];
+const tail = (arr) => arr.slice(1);
+const init = (arr) => arr.slice(0, arr.length - 1);
+const last = (arr) => arr[arr.length - 1];
+
+console.log(head([1,2,3,4,5])); // => 1
+console.log(tail([1,2,3,4,5])); // => [2,3,4,5]
+console.log(init([1,2,3,4,5])) // => [1,2,3,4]
+console.log(last([1,2,3,4,5])) // => 5
